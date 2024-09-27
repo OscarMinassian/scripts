0e29c2c5ff25a82e926d638e71deedcd0f6271f1
Oscar Minassian <therealoscarminassian@gmail.com>
2024-09-28 09:18:23 +1000

    Added params, commented some prints out and created a formatted JSON string responce

---
+++ dazopsScript.py
@@ -0,0 +1,23 @@
+# Pull data from the API and return the current temp for Bronty
+import requests
+import json
+
+# Params for weather I'm interested in. 
+# Long/Lat set to Bronty NSW
+
+longitude = 151.26
+latitude = -33.9
+timezone = "Australia/NSW"
+forecast_days = 3
+temperature_2m = "°C"
+
+response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&timezone={timezone}&forecast_days={forecast_days}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+# print(response.status_code)
+# print(response.json())
+
+# create a formatted string of the Python JSON object
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint(response.json())
